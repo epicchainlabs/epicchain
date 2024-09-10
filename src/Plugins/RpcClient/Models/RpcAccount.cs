4c45f6c4e9fbb88a12f765a2f006cc08a1a1231e
xmoohad <snipperrr@yahoo.com>
2024-09-10 11:19:06 +0200

    EpicChain Core - EpicChain Lab's

--- src/Plugins/RpcClient/Models/RpcAccount.cs
+++ src/Plugins/RpcClient/Models/RpcAccount.cs
@@ -1,13 +1,23 @@
-// Copyright (C) 2015-2024 The Neo Project.
+// Copyright (C) 2021-2024 EpicChain Labs.
+
 //
-// RpcAccount.cs file belongs to the neo project and is free
-// software distributed under the MIT software license, see the
-// accompanying file LICENSE in the main directory of the
-// repository or http://www.opensource.org/licenses/mit-license.php
-// for more details.
+// RpcAccount.cs is a component of the EpicChain Labs project, founded by xmoohad. This file is
+// distributed as free software under the MIT License, allowing for wide usage and modification
+// with minimal restrictions. For comprehensive details regarding the license, please refer to
+// the LICENSE file located in the root directory of the repository or visit
+// http://www.opensource.org/licenses/mit-license.php.
 //
-// Redistribution and use in source and binary forms with or without
-// modifications are permitted.
+// EpicChain Labs is dedicated to fostering innovation and development in the blockchain space,
+// and we believe in the open-source philosophy as a way to drive progress and collaboration.
+// This file, along with all associated code and documentation, is provided with the intention of
+// supporting and enhancing the development community.
+//
+// Redistribution and use of this file in both source and binary forms, with or without
+// modifications, are permitted. We encourage users to contribute to the project and respect the
+// guidelines outlined in the LICENSE file. By using this software, you agree to the terms and
+// conditions specified in the MIT License, ensuring the continuation of free and open software
+// practices.
+
 
 using Neo.Json;
 
